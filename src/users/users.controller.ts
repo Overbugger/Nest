61da7852372e05bc47c98f0d583b748b4c6c31ba
Sammie4068 <okorosamuelchika@gmail.com>
2025-01-19 17:27:50 +0100

    lesson 3

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,31 +8,34 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService, User, Role } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.userService.findAll(role);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.userService.findOne(+id);
   }
 
   @Post()
-  create(@Body() user: {}) {
-    return user;
+  create(@Body() user: User) {
+    return this.userService.create(user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(@Param('id') id: string, @Body() userUpdate: Partial<User>) {
+    return this.userService.update(+id, userUpdate);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return { id };
+    return this.userService.delete(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,79 @@
 import { Injectable } from '@nestjs/common';
 
+export type Role = 'INTERN' | 'ENGINEER' | 'ADMIN';
+
+export interface User {
+  name: string;
+  email: string;
+  role: Role;
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Sam Amadi',
+      email: 'sam@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Sam Alabi',
+      email: 'sam@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Sam Audu',
+      email: 'sam@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Sam Awilo',
+      email: 'sam@gmail.com',
+      role: 'ADMIN',
+    },
+  ];
+
+  findAll(role?: Role) {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
+  create(user: User) {
+    const newUserId = [...this.users].sort((a, b) => b.id - a.id)[0].id + 1;
+    const newUser = {
+      id: newUserId,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, updatedUser: Partial<User>) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return `User ${id} Removed`;
+  }
+}
