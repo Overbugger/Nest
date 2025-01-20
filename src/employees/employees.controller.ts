cc7968246e1f205d41aa9672f7d221b5e4acc765
Sammie4068 <okorosamuelchika@gmail.com>
2025-01-20 09:31:51 +0100

    The end

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -9,20 +9,22 @@ import {
   Query,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
-import { Prisma } from '@prisma/client';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { Prisma, Role } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @SkipThrottle({ default: false })
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
 
   @Get()
-  findAll(@Query('role') role?: CreateUserDto['role']) {
+  findAll(@Query('role') role?: Role) {
     return this.employeesService.findAll(role);
   }
 
@@ -31,6 +33,7 @@ export class EmployeesController {
     return this.employeesService.findOne(+id);
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } })
   @Patch(':id')
   update(
     @Param('id') id: string,
---
+++ src/my-logger/my-logger.service.ts
@@ -0,0 +1,22 @@
+// import { ConsoleLogger, Injectable } from '@nestjs/common';
+// import * as fs from 'fs'
+// import { promises as fsPromises } from 'fs'
+// import * as path from 'path'
+
+// @Injectable()
+// export class MyLoggerService extends ConsoleLogger {
+//     log(message: any, context?: string) {
+//         async logTofile(entry){
+//             const formattedEntry = `${intl}`
+//         }
+//         const entry = `${context}\t${message}`
+
+//         super.log(message, context)
+//     }
+
+//     error(message: any, stackOrContext?: string) {
+//          const entry = `${stackOrContext}\t${message}`
+
+//         super.error(message, stackOrContext)
+//     }
+// }
